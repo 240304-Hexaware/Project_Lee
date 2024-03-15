42160fd9ade868f0ecae0b982d094a648c9d0912
Joonwoo Lee <joonwoo509@revature.net>
2024-03-15 17:39:18 -0400

    removed token in parameters because token will be sent via cookie

--- src/main/java/com/revature/project/parser/payload/response/LoginResponse.java
+++ src/main/java/com/revature/project/parser/payload/response/LoginResponse.java
@@ -1,5 +1,5 @@
 package com.revature.project.parser.payload.response;
 
-public record LoginResponse(String userId, String username, boolean isAdmin, String token) {
+public record LoginResponse(String userId, String username, boolean isAdmin) {
 
 }

36fd9cca13d54898db4d0b3d2dd7523bc14a3ab0
Joonwoo Lee <joonwoo509@revature.net>
2024-03-25 17:38:39 -0400

    removed userId in parameters because userId will be extracted from token

--- src/main/java/com/revature/project/parser/payload/request/TaskRequest.java
+++ src/main/java/com/revature/project/parser/payload/request/TaskRequest.java
@@ -3,8 +3,7 @@ package com.revature.project.parser.payload.request;
 import jakarta.validation.constraints.NotNull;
 
 public record TaskRequest(
-        @NotNull(message = "userId must be present") String userId,
-        @NotNull(message = "rawFileId must be present") String rawFileId,
-        @NotNull(message = "specId must be present") String specId) {
+                @NotNull(message = "rawFileId must be present") String rawFileId,
+                @NotNull(message = "specId must be present") String specId) {
 
 }

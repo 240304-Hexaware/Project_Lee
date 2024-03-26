7c7ff0c6d9c72d785df59966d17e979690744c72
Joonwoo Lee <joonwoo509@revature.net>
2024-03-26 12:53:19 -0400

    made it so that user profile component only shows up when user is logged in

--- frontend/src/app/components/user-profile/user-profile.component.ts
+++ frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
-export class UserProfileComponent {
-  public username: string = '';
+export class UserProfileComponent implements OnInit {
+  username?: string;
+  on: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {
-    // FIXME: after a successful logout, username is not cleared
-    this.username = this.authService.getCurrentUserUsername();
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe((user) => {
+      this.username = user?.username;
+      this.on = !!user;
+    });
   }
-
-  // TODO: it only shows up when user is logged in
 
   logout() {
     this.authService.logout();
+    this.on = false;
     this.router.navigate(['/login']);
   }
 }

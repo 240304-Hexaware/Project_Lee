ec8df007bf13dad3fb5e19b8881d799bf8ab6d6e
Joonwoo Lee <joonwoo509@revature.net>
2024-03-29 21:55:54 -0400

    completed register page

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,44 +1,82 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
-import { RegisterRequestParams } from '../../utils/types';
+import { ErrorResponse, RegisterRequestParams } from '../../utils/types';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, RouterLink],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  username: string = '';
-  password: string = '';
-  confirmPassword: string = '';
+  registerForm = this.formBuilder.group({
+    username: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(3)]),
+    ],
+    password: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(3)]),
+    ],
+    confirmPassword: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(3)]),
+    ],
+  });
   error: string = '';
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
 
   register(): void {
-    const params: RegisterRequestParams = {
-      username: this.username,
-      password: this.password,
-      confirmPassword: this.confirmPassword,
-    };
+    if (!this.validate()) {
+      return;
+    }
 
-    this.authService.register(params).subscribe({
-      next: (_) => {
-        this.router.navigate(['/login']);
-      },
-      error: (error) => {
-        if (error instanceof Error) {
-          console.error(error.message);
-        } else {
-          console.error(error);
-        }
-        this.error = 'Something went wrong! Try again.';
-      },
-    });
+    this.authService
+      .register({ ...this.registerForm.value } as RegisterRequestParams)
+      .subscribe({
+        next: (_) => {
+          this.router.navigate(['/login']);
+        },
+        error: (error: ErrorResponse) => {
+          this.updateError(error.title || 'Something went wrong! Try again.');
+        },
+      });
+  }
+
+  validate(): boolean {
+    const { password, confirmPassword } = this.registerForm.value;
+    if (password !== confirmPassword) {
+      this.updateError('Passwords do not match');
+      return false;
+    }
+    if (!this.registerForm.valid) {
+      this.updateError('Enter valid input');
+      return false;
+    }
+    return true;
+  }
+
+  updateError(msg: string) {
+    this.error = msg;
   }
 }
